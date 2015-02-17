00785ac33bc17709c0d68edf6d495e42879ff450
Kenpa <tchianphou@gmail.com>
2015-02-17 14:34:21 +0100

    debug routing
    dropdown links still buggy

--- app/components/category/category.config.js
+++ app/components/category/category.config.js
@@ -2,9 +2,9 @@
     
     angular.module('category').config(['$routeProvider', function($routeProvider) {
         $routeProvider.
-            when('/category/:param', {
-                templateUrl: '/app/components/category/category.html',
-                controller: 'RouteController'
+            when('/:param', {
+                templateUrl: '/app/components/category/category-specific/category-specific.html',
+                controller: 'RouteCategoryController'
             });
     }]);
 })();
--- app/components/category/category.controller.js
+++ app/components/category/category.controller.js
@@ -1,5 +1,25 @@
 (function() {
     angular.module('category').controller("RouteCategoryController", function($scope, $routeParams) {
         $scope.routeCat = $routeParams.param;
+        switch($routeParams.param) {
+            case 'hightech':
+                 $scope.labelCat = 'High-Tech';
+                 break;
+            case 'voyage':
+                 $scope.labelCat = 'Voyages';
+                 break;
+            case 'loisirs':
+                 $scope.labelCat = 'Loisirs';
+                 break;
+            case 'electromenager':
+                 $scope.labelCat = 'Electroménager';
+                 break;
+            case 'mode':
+                 $scope.labelCat = 'Mode';
+                 break;
+                
+            default:
+        }
+       
     });
 })();
