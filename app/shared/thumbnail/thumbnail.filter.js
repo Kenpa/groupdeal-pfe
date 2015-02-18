9cedaed682f7beead94f382294f1360b9ccb63bc
Kenpa <tchianphou@gmail.com>
2015-02-18 02:44:04 +0100

    show similar product : semi fonctionnal
    need to work on the tagsMatch filter function

--- app/shared/thumbnail/thumbnail.filter.js
+++ app/shared/thumbnail/thumbnail.filter.js
@@ -30,4 +30,37 @@
         };
     });
     
+    
+    angular.module('thumbnail').filter('tagsMatch', function() {
+        return function(items, idProd) {
+            
+            var filtered = [];
+            for (var i = 0; i < items.length; i++) 
+            {
+                var item = items[i];  
+                var j = 0;
+                var notPush = true;
+                
+                while (j < items[idProd].tags.length && notPush)
+                {
+                    var k=0;
+                    while (k < item.tags.length) {
+                        if (items[idProd].tags[j] != item.tags[k])
+                            k++;
+                        else
+                            k = item.tags.length+1
+                    }
+                    
+                    if (k = item.tags.length+1){
+                        filtered.push(item);
+                        notPush = false;
+                    }
+                    else
+                        j++;
+                }              
+            }
+            return filtered;
+        };
+    });
+    
 })();
